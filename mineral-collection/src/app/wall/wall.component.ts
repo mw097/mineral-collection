efaa8be3eaf6e50860d27d203a1f934dcc866834
mw097 <mihelone@gmail.com>
2025-05-29 17:13:26 +0200

    add: prettier

--- mineral-collection/src/app/wall/wall.component.ts
+++ mineral-collection/src/app/wall/wall.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import  {MatSidenavModule } from '@angular/material/sidenav';
+import { ContentCardComponent } from '../shared/content-card/content-card.component';
 
 @Component({
   selector: 'app-wall',
   imports: [
     MatCardModule,
     MatSidenavModule,
+    ContentCardComponent,
   ],
   templateUrl: './wall.component.html',
   styleUrl: './wall.component.scss'
