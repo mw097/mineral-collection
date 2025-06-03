f4744de62c16bfd1b27ef29969a3b191b2c5499a
mw097 <mihelone@gmail.com>
2025-06-03 16:39:22 +0200

    add: card animations

--- mineral-collection/src/app/app-routing.module.ts
+++ mineral-collection/src/app/app-routing.module.ts
@@ -1,9 +1,37 @@
-import {  Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
 export const routes: Routes = [
-  { path: 'meteorite/:specimenId', loadComponent: () => import('./specimen-page/specimen-page.component').then(m => m.SpecimenPageComponent) },
-  { path: 'minerals', loadComponent: () => import('./mineral-list/mineral-list.component').then(m => m.MineralListComponent) },
-  { path: 'meteorites', loadComponent: () => import('./meteorite-list/meteorite-list.component').then(m => m.MeteoriteListComponent) },
-  { path: 'about', loadComponent: () => import('./about/about.component').then(m => m.AboutComponent)},
-  { path: 'wall', loadComponent: () => import('./wall/wall.component').then(m => m.WallComponent)},
-];
+  {
+    path: 'meteorite/:specimenId',
+    loadComponent: () =>
+      import('./specimen-page/specimen-page.component').then(
+        (m) => m.SpecimenPageComponent,
+      ),
+  },
+  {
+    path: 'minerals',
+    loadComponent: () =>
+      import('./mineral-list/mineral-list.component').then(
+        (m) => m.MineralListComponent,
+      ),
+  },
+  {
+    path: 'meteorites',
+    loadComponent: () =>
+      import('./meteorite-list/meteorite-list.component').then(
+        (m) => m.MeteoriteListComponent,
+      ),
+  },
+  {
+    path: 'about',
+    loadComponent: () =>
+      import('./about/about.component').then((m) => m.AboutComponent),
+  },
+  {
+    path: 'wall',
+    loadComponent: () =>
+      import('./wall/wall.component').then((m) => m.WallComponent),
+  },
+  { path: '', redirectTo: '/wall', pathMatch: 'full' },
+  { path: '**', redirectTo: '/wall' },
+]
--- mineral-collection/src/app/shared/mineral-label/mineral-label.component.ts
+++ mineral-collection/src/app/shared/mineral-label/mineral-label.component.ts
@@ -1,12 +1,41 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { MineralData } from 'src/app/constants/types';
+import { CommonModule } from '@angular/common';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  group,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-mineral-label',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './mineral-label.component.html',
-  styleUrl: './mineral-label.component.scss'
+  styleUrl: './mineral-label.component.scss',
+  animations: [
+    trigger('hoverAnimation', [
+      state('default', style({ transform: 'scale(1)' })),
+      state('hovered', style({ transform: 'scale(1.05)' })),
+      transition('default => hovered', [
+        group([animate('200ms ease-out', style({ transform: 'scale(1.05)' }))]),
+      ]),
+      transition('hovered => default', [
+        group([animate('200ms ease-in', style({ transform: 'scale(1)' }))]),
+      ]),
+    ]),
+  ],
 })
 export class MineralLabelComponent {
   readonly mineralData = input.required<MineralData>();
+  readonly isHovered = signal(false);
+
+  /**
+   * Toggles the visibility of the mineral details.
+   */
+  setHover(shouldDisplay: boolean) {
+    this.isHovered.set(shouldDisplay);
+  }
 }
