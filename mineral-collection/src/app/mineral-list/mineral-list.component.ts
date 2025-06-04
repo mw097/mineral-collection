c42b96401fa49c8ed66eb775ba24b233e40c86c3
mw097 <mihelone@gmail.com>
2025-06-04 22:21:13 +0200

    add: description

--- mineral-collection/src/app/mineral-list/mineral-list.component.ts
+++ mineral-collection/src/app/mineral-list/mineral-list.component.ts
@@ -4,20 +4,27 @@ import { toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
 import { MineralData } from '../constants/types';
 import { MineralLabelComponent } from '../shared/mineral-label/mineral-label.component';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
-    selector: 'app-mineral-list',
-    templateUrl: './mineral-list.component.html',
-    styleUrls: ['./mineral-list.component.scss'],
-    standalone: true,
-    imports: [
-      CommonModule, 
-      MineralLabelComponent,
-    ],
+  selector: 'app-mineral-list',
+  templateUrl: './mineral-list.component.html',
+  styleUrls: ['./mineral-list.component.scss'],
+  standalone: true,
+  imports: [CommonModule, MineralLabelComponent],
 })
 export class MineralListComponent {
   private readonly mineralService = inject(MineralService);
   protected readonly minerals: Signal<MineralData[]> = toSignal(
-    this.mineralService.getMinerals(), {initialValue: []}
+    this.mineralService
+      .getMinerals()
+      .pipe(
+        map((list) =>
+          list.sort((a: MineralData, b: MineralData) =>
+            a.id.localeCompare(b.id, undefined, { numeric: true })
+          )
+        )
+      ),
+    { initialValue: [] }
   );
 }
